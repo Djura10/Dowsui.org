7da9dd89045750afe2102a2b0d8ee7993b4185d1
Djura10 <gjergj.milici@gmail.com>
2025-03-31 21:21:25 +0200

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Willkommen bei Dowsui.org</h1>
+      <p>Setze dir Ziele, investiere Tokens und bleib fokussiert!</p>
+    </div>
+  );
+}
